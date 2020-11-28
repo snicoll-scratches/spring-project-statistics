187df51d0846e84ee41d2267545747976e9c0e58
Stephane Nicoll <snicoll@vmware.com>
2020-11-28 18:17:40 +0100

    Polish

--- src/main/java/io/spring/stats/SampleApplication.java
+++ src/main/java/io/spring/stats/SampleApplication.java
@@ -20,24 +20,20 @@ public class SampleApplication {
 			ProjectCreationStatistics projectCreationStatistics) {
 		return args -> {
 			Period quarter = Period.ofMonths(3);
+			LocalDate q1 = LocalDate.of(2020, 1, 1);
+			LocalDate q2 = q1.plus(quarter);
+			LocalDate q3 = q2.plus(quarter);
+			LocalDate q4 = q3.plus(quarter);
 			System.out.println("Maven Central statistics for Spring Batch (spring-batch-core):");
-			System.out.println("Q1 - " + mavenCentralStatistics.getDownloadCount(
-					"spring-batch", "spring-batch-core", LocalDate.of(2020, 1, 1), quarter));
-			System.out.println("Q2 - " + mavenCentralStatistics.getDownloadCount(
-					"spring-batch", "spring-batch-core", LocalDate.of(2020, 4, 1), quarter));
-			System.out.println("Q3 - " + mavenCentralStatistics.getDownloadCount(
-					"spring-batch", "spring-batch-core", LocalDate.of(2020, 7, 1), quarter));
-			System.out.println("Q4 - " + mavenCentralStatistics.getDownloadCount(
-					"spring-batch", "spring-batch-core", LocalDate.of(2020, 10, 1), quarter));
+			System.out.println("Q1 - " + mavenCentralStatistics.getDownloadCount("spring-batch", "spring-batch-core", q1, quarter));
+			System.out.println("Q2 - " + mavenCentralStatistics.getDownloadCount("spring-batch", "spring-batch-core", q2, quarter));
+			System.out.println("Q3 - " + mavenCentralStatistics.getDownloadCount("spring-batch", "spring-batch-core", q3, quarter));
+			System.out.println("Q4 - " + mavenCentralStatistics.getDownloadCount("spring-batch", "spring-batch-core", q4, quarter));
 			System.out.println("Project Generation statistics for Spring Batch:");
-			System.out.println("Batch Q1 - " + projectCreationStatistics.getProjectCreationCount(
-					"batch", LocalDate.of(2020, 1, 1), quarter));
-			System.out.println("Batch Q2 - " + projectCreationStatistics.getProjectCreationCount(
-					"batch", LocalDate.of(2020, 4, 1), quarter));
-			System.out.println("Batch Q3 - " + projectCreationStatistics.getProjectCreationCount(
-					"batch", LocalDate.of(2020, 7, 1), quarter));
-			System.out.println("Batch Q4 - " + projectCreationStatistics.getProjectCreationCount(
-					"batch", LocalDate.of(2020, 10, 1), quarter));
+			System.out.println("Q1 - " + projectCreationStatistics.getProjectCreationCount("batch", q1, quarter));
+			System.out.println("Q2 - " + projectCreationStatistics.getProjectCreationCount("batch", q2, quarter));
+			System.out.println("Q3 - " + projectCreationStatistics.getProjectCreationCount("batch", q3, quarter));
+			System.out.println("Q4 - " + projectCreationStatistics.getProjectCreationCount("batch", q4, quarter));
 		};
 	}
 
