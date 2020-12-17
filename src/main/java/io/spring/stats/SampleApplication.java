df59d0db2a51f91b329efcd1ffd0cef7973cdf0c
Stephane Nicoll <snicoll@vmware.com>
2020-12-17 15:26:43 +0100

    Make the runner more configurable

--- src/main/java/io/spring/stats/SampleApplication.java
+++ src/main/java/io/spring/stats/SampleApplication.java
@@ -16,16 +16,19 @@ public class SampleApplication {
 	public ApplicationRunner statisticsRunner(MavenCentralStatistics mavenCentralStatistics,
 			ProjectCreationStatistics projectCreationStatistics) {
 		return args -> {
+			String projectId = "spring-batch";
+			String artifactId = "spring-batch-core";
+			String dependencyId = "batch";
 			int year = 2020;
-			System.out.println("Maven Central statistics for Spring Batch (spring-batch-core):");
-			for (int month = 1; month <= 12; month++) {
-				System.out.printf("%s-%02d - %s%n", year, month, mavenCentralStatistics
-						.getMonthlyDownloadCount("spring-batch", "spring-batch-core", year, month));
-			}
-			System.out.println("Project Generation statistics for Spring Batch:");
+			System.out.printf("Maven Central statistics for %s (%s):%n", projectId, artifactId);
 			for (int month = 1; month <= 12; month++) {
 				System.out.printf("%s-%02d - %s%n", year, month,
-						projectCreationStatistics.getMonthlyProjectCreationCount("batch", year, month));
+						mavenCentralStatistics.getMonthlyDownloadCount(projectId, artifactId, year, month));
+			}
+			System.out.printf("Project Generation statistics for %s (%s):%n", projectId, dependencyId);
+			for (int month = 1; month <= 12; month++) {
+				System.out.printf("%s-%02d - %s%n", year, month,
+						projectCreationStatistics.getMonthlyProjectCreationCount(dependencyId, year, month));
 			}
 		};
 	}
