907a50e93abf55e38d5788766bdec171bef48b90
Ryan Baxter <rbaxter@vmware.com>
2021-01-12 16:52:08 -0500

    Support totals for multiple artifact ids and dependency ids

--- src/main/java/io/spring/stats/SampleApplication.java
+++ src/main/java/io/spring/stats/SampleApplication.java
@@ -17,18 +17,25 @@ public class SampleApplication {
 			ProjectCreationStatistics projectCreationStatistics) {
 		return args -> {
 			String projectId = "spring-batch";
-			String artifactId = "spring-batch-core";
-			String dependencyId = "batch";
+			String[] artifactIds = {"spring-batch-core"};
+			String[] dependencyIds = {"batch"};
 			int year = 2020;
-			System.out.printf("Maven Central statistics for %s (%s):%n", projectId, artifactId);
+			System.out.printf("Maven Central statistics for %s (%s):%n", projectId, String.join(", ", artifactIds));
 			for (int month = 1; month <= 12; month++) {
-				System.out.printf("%s-%02d - %s%n", year, month,
-						mavenCentralStatistics.getMonthlyDownloadCount(projectId, artifactId, year, month));
+				long total = 0;
+				for (String artifactId: artifactIds) {
+					total = total + mavenCentralStatistics.getMonthlyDownloadCount(projectId, artifactId, year, month);
+				}
+				System.out.printf("%s-%02d - %s%n", year, month, total);
 			}
-			System.out.printf("Project Generation statistics for %s (%s):%n", projectId, dependencyId);
+			System.out.printf("Project Generation statistics for %s (%s):%n", projectId, String.join(", ", dependencyIds));
+
 			for (int month = 1; month <= 12; month++) {
-				System.out.printf("%s-%02d - %s%n", year, month,
-						projectCreationStatistics.getMonthlyProjectCreationCount(dependencyId, year, month));
+				int total = 0;
+				for (String dependencyId : dependencyIds) {
+					total += projectCreationStatistics.getMonthlyProjectCreationCount(dependencyId, year, month);
+				}
+				System.out.printf("%s-%02d - %s%n", year, month, total);
 			}
 		};
 	}
